50b5375239cc6ed8bb07b98e8995444ba40d935f
SK052165 <sk052165@cerner.net>
2022-07-14 16:11:04 +0530

    Initial commit

---
+++ src/components/Main/Main.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './Main.module.css';
+
+const Main = () => (
+  <div className={styles.Main}>
+    Main Component
+  </div>
+);
+
+Main.propTypes = {};
+
+Main.defaultProps = {};
+
+export default Main;
